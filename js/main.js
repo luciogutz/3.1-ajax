2e39842cb5a3336d2bb12b62227691596215909e
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-12 14:11:16 -0600

    was able to retrieve data from git api

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-document.addEventListener("DOMContentLoader", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
 
+  var githubElement = document.querySelector("[data-js='github']");
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/users/luciogutz/repos");
+  xhr.addEventListener("load", function(e){
+
+      var xhrData = this.response;
+
+      var JSONData = JSON.parse(xhrData);
+
+      var searchArray = JSONData.commits;
+        // console.log(JSONData);
+  });
 
 
-
-
-
+xhr.send();
 
 });
